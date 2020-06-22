6662e2e673dc94604998c2fdf635ef3785caa656
Bryce Whittington <brycew30@gmail.com>
2020-06-22 17:47:45 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -46,7 +46,6 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   holiday_hash[:winter][:bryce_birthday] = ["cookie cake", "presents", "movie"]
   # remember to return the updated hash
   holiday_hash
-
 end
 
 def all_winter_holiday_supplies(holiday_hash)
